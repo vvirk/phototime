6d22b00c1292b4cfd872b02d1f81ae818dfb690a
Boxing199 <virkolomeo@gmail.com>
2019-01-29 00:59:03 +0200

    adedd community

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,6 @@
 $('.design__sl').slick({
-    prevArrow: "<i class='fa fa-angle-left design__sl-prev' aria-hidden='true'></i>",
-    nextArrow: "<i class='fa fa-angle-right design__sl-next' aria-hidden='true'></i>",
+    prevArrow: "<i class='fa fa-angle-left prev design__sl-prev' aria-hidden='true'></i>",
+    nextArrow: "<i class='fa fa-angle-right next design__sl-next' aria-hidden='true'></i>",
     autoplay: true,
     autoplaySpeed: 7000,
     responsive: [
@@ -10,5 +10,26 @@ $('.design__sl').slick({
               arrows: false
             }
       },
-  ]
+    ]
+  });
+
+  $('.community__sl').slick({
+    slidesToShow: 2,
+    prevArrow: "<i class='fa fa-angle-left prev community__sl-prev' aria-hidden='true'></i>",
+    nextArrow: "<i class='fa fa-angle-right next community__sl-next' aria-hidden='true'></i>",
+    responsive: [
+        {
+            breakpoint: 801,
+            settings: {
+                slidesToShow: 1
+              }
+        },
+        {
+            breakpoint: 601,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+              }
+        },
+    ]
   });
